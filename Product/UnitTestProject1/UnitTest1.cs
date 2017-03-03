008a5bc05815b6123a11b0d749561d7650875997
riholay <riholay@gmail.com>
2017-03-03 23:58:26 +0100

    functionnal basis

--- Product/UnitTestProject1/UnitTest1.cs
+++ Product/UnitTestProject1/UnitTest1.cs
@@ -6,9 +6,19 @@ namespace UnitTestProject1
     [TestClass]
     public class UnitTest1
     {
+
+        public TestContext TestContext { get; set; }
+
+        [ClassInitialize]
+        public static void Initialize(TestContext testContext)
+        {
+
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
+            Assert.IsTrue(true);
         }
     }
 }
